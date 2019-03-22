f6ff1729ab853ad091189bd9c11751517a4b30dc
Mateusz Malek-Podjaski <2323841m@student.gla.ac.uk>
2019-03-22 01:18:50 +0000

    Added custom template tags and cleaned up html for post and categories

--- meansunz_project/meansunz/templatetags/meansunz_template_tags.py
+++ meansunz_project/meansunz/templatetags/meansunz_template_tags.py
@@ -5,3 +5,15 @@ register = template.Library()
 @register.filter
 def sub(val1, val2):
     return val1 - val2
+
+@register.simple_tag
+def get_user_rating(profile=None):
+    return profile.rating_comment + profile.rating_post
+
+@register.simple_tag
+def get_post_votes(post=None):
+    return post.upvotes - post.downvotes
+
+@register.simple_tag
+def get_comment_votes(comment=None):
+    return comment.upvotes - comment.downvotes
